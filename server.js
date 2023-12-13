0e2f0c4b5a27db2ac0895f60db7d84d074579b43
V1KHROV <dmvikhrov@hotmail.com>
2023-12-13 11:59:27 +0300

    Add simple webclient

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-import ws from 'ws';
+import { WebSocketServer } from 'ws';
 import {v4 as uuid} from 'uuid';
 
-const {Server} = ws;
+const host = 'localhost';
+const port = 8000;
 
+// clients objects
 const clients = {};
-const wss = new Server({port: 8000});
+
+const wss = new WebSocketServer({host: host, port: port});
+console.log(`server start on ${host}:${port}`);
 wss.on("connection", (ws) => {
   const id = uuid();
   clients[id] = ws;
@@ -12,7 +16,7 @@ wss.on("connection", (ws) => {
   console.log(`new client: ${id}`); 
 
   ws.on('message', (rawMessage) => {
-
+    console.log(`message: ${rawMessage}`);
   });
 
   ws.on('close', () => {
